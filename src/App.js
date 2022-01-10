6425552450c95274a16cf89a4c4a9f4de2084a61
Atte Koivukangas <atte.koivukangas2@gmail.com>
2022-01-10 20:56:09 +0200

    Grid input now blurs when input size hits 16 characters

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import { Container, FormControl } from 'react-bootstrap';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import SolutionList from './components/SolutionList';
 import UsageGuide from './components/UsageGuide/UsageGuide';
 
 const App = () => {
   const [userInput, setUserInput] = useState('');
+  const inputRef = useRef();
+
+  const handleUserInputChange = ({ target }) => {
+    setUserInput(target.value);
+
+    if (target.value.length === 16) {
+      inputRef.current.blur();
+    }
+  }
 
   return (
     <Container className='mt-3'>
       <UsageGuide />
       <FormControl
+        ref={inputRef}
         className='mb-3'
         placeholder='Syötä kirjaimet'
         maxLength={16}
-        onChange={({ target }) => setUserInput(target.value)}
+        onChange={handleUserInputChange}
         value={userInput}
       />
       <SolutionList userInput={userInput} />
