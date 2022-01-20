722b69a492bfb82f03d9249e4565a81e3a54c0ae
Atte Koivukangas <atte.koivukangas2@gmail.com>
2022-01-20 14:42:47 +0200

    Updated SolutionList and Copyright styles

--- src/components/SolutionList/SolutionList.js
+++ src/components/SolutionList/SolutionList.js
@@ -3,6 +3,7 @@ import { groupSolutions } from './SolutionListUtils';
 import useSolverWorker from './useSolverWorker';
 import PropTypes from 'prop-types';
 import { store, actions } from '../../store';
+import './SolutionList.css';
 
 const propTypes = {
   userInput: PropTypes.string.isRequired
@@ -27,8 +28,10 @@ const SolutionList = ({ userInput }) => {
         ? <h3>Etsitään erilaisia sanakompinaatioita...</h3>
         : groupSolutions(state.solutions).map(({ length, words }) =>
             <div key={length}>
-              <h3>{length} kirjaimiset sanat</h3>
-              <p>{words.join(', ')}</p>
+              <h3 className='word-container-title'>{length} kirjaimiset sanat</h3>
+              <p className='word-container'>
+                {words.map(word => word.charAt(0)+word.slice(1).toLowerCase()).join(', ')}
+              </p>
             </div>
           )
         }
